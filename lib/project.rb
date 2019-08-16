5221e98578fa9da4245b97f34f8bc1e5bbea7c73
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-08-16 14:32:02 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -14,7 +14,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self) unless 
+    backer.back_project(self) unless backer.back_project.include(self)
   end 
   
 end 
